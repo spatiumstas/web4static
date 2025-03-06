ac9d0425e24f76945a0bf7660ae608d761b1c1ce
spatiumstas <79056064+spatiumstas@users.noreply.github.com>
2025-03-06 09:12:02 +0300

    use native extension #7

--- files/web4static.php
+++ files/web4static.php
@@ -177,7 +177,7 @@ if (isset($_GET['export_all'])) {
     $tempDir = sys_get_temp_dir() . '/web4static_backup_' . time();
     mkdir($tempDir, 0777, true);
     foreach ($files as $key => $path) {
-        $extension = pathinfo($path, PATHINFO_EXTENSION);м
+        $extension = pathinfo($path, PATHINFO_EXTENSION);
         $backupFile = $tempDir . '/' . $key . ($extension ? '.' . $extension : '');
         file_put_contents($backupFile, $texts[$key]);
     }
