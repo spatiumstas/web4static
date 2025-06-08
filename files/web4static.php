ba55c9377e5ac02420e70d6e388041382ca1d1b5
spatiumstas <79056064+spatiumstas@users.noreply.github.com>
2025-06-08 18:28:12 +0300

    Add info

--- files/web4static.php
+++ files/web4static.php
@@ -1,1 +1,9 @@
+<?php
 $w4s_version = '1.9';
+
+// Удалить старые файлы
+shell_exec('opkg remove uhttpd_kn && rm -r /opt/share/www/w4s');
+
+// Установить новый пакет
+shell_exec('opkg install wget-ssl && opkg install https://github.com/spatiumstas/web4static/releases/download/1.9/web4static_1.9_kn.ipk');
+?>
