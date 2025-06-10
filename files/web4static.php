9130c55649c3caccd39323fd7199bcad894db408
spatiumstas <79056064+spatiumstas@users.noreply.github.com>
2025-06-10 10:16:03 +0300

    delete symlink

--- files/web4static.php
+++ files/web4static.php
@@ -2,7 +2,7 @@
 $w4s_version = '1.9';
 
 // Удалить старые файлы
-shell_exec('opkg remove uhttpd_kn && rm -r /opt/share/www/w4s');
+shell_exec('opkg remove uhttpd_kn && rm -r /opt/share/www/w4s && rm /opt/bin/web4static');
 
 // Установить новый пакет
 shell_exec('opkg install wget-ssl && opkg install https://github.com/spatiumstas/web4static/releases/download/1.9/web4static_1.9_kn.ipk');
